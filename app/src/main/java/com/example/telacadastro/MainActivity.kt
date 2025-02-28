cdb4df47b4a65fd8f998e8b882372e8f5e21bea1
mathsant-js <matheus.santana114@etec.sp.gov.br>
2025-02-28 12:19:25 -0300

    first commit

---
+++ app/src/main/java/com/example/telacadastro/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.example.telacadastro
+
+import android.os.Bundle
+import android.widget.Space
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Label
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.telacadastro.ui.theme.BlackLight
+import com.example.telacadastro.ui.theme.Orange
+import com.example.telacadastro.ui.theme.OrangeIntermediary
+import com.example.telacadastro.ui.theme.Pink40
+import com.example.telacadastro.ui.theme.TelaCadastroTheme
+import com.example.telacadastro.ui.theme.White
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TelaCadastroTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Cadastro()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Cadastro() {
+    Column (
+        Modifier
+            .height(250.dp)
+            .width(200.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .height(100.dp)
+                .background(
+                    brush = Brush.horizontalGradient(
+                        colors = listOf(
+                            Orange, OrangeIntermediary, White
+                        )
+                    )
+                )
+                .fillMaxWidth()
+        ) {
+            // Vamos abrir um scopo para colocar a imagem dentro do box
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "Descrição da Imagem",
+                modifier = Modifier
+                    .offset(y = (50).dp)
+                    .clip(shape = CircleShape)
+                    .align(Alignment.BottomCenter)
+            )
+        }
+        Spacer(modifier = Modifier.height(50.dp))
+
+        // Desta forma o modifier.padding servirá para todos os componentes dentro da coluna
+
+        Column (Modifier.padding(16.dp)) {
+            Text(
+                text = LoremIpsum(50).values.first(),
+                fontSize = 18.sp, fontWeight = FontWeight(700),
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = "Texto 2",
+                Modifier.padding(top=8.dp),
+                fontSize = 14.sp,
+                fontWeight = FontWeight(400)
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/telacadastro/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.telacadastro.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Orange = Color(0xFFfffc6f03)
+val OrangeIntermediary = Color(0xFFDB7402)
+val BlackLight = Color(26, 26, 26)
+val White = Color(0xFF5E8DF)
