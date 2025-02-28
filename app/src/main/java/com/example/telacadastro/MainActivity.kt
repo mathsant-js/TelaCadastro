a892ee00757e57f0d48a7c8a64791daff5fc2075
mathsant-js <matheus.santana114@etec.sp.gov.br>
2025-02-28 12:27:35 -0300

    feat: functions for labels and textfield has been added

--- app/src/main/java/com/example/telacadastro/MainActivity.kt
+++ app/src/main/java/com/example/telacadastro/MainActivity.kt
@@ -20,13 +20,19 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Label
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
@@ -102,4 +108,56 @@ fun Cadastro() {
             )
         }
     }
+}
+
+@Composable
+fun Nome() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = text,
+        onValueChange = {
+            text = it
+        },
+        label = { Text(text = "Nome") },
+        placeholder = { Text(text = "Digite o seu nome") },
+    )
+}
+
+@Composable
+fun Telefone() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = text,
+        onValueChange = {
+            text = it
+        },
+        label = { Text(text = "Telefone") },
+        placeholder = { Text(text = "Digite o seu telefone") },
+    )
+}
+
+@Composable
+fun Curso() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = text,
+        onValueChange = {
+            text = it
+        },
+        label = { Text(text = "Curso") },
+        placeholder = { Text(text = "Digite o seu curso") },
+    )
+}
+
+@Composable
+fun Serie() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = text,
+        onValueChange = {
+            text = it
+        },
+        label = { Text(text = "Série") },
+        placeholder = { Text(text = "Digite a sua série") },
+    )
 }
